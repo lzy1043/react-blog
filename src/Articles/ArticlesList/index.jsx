8b3e9b2b58b751e61a24cf38d7795aecea5688df
liuziyang <798230885@qq.com>
2018-02-27 23:05:18 +0800

    add articles list component

--- src/Articles/ArticlesList/index.jsx
+++ src/Articles/ArticlesList/index.jsx
@@ -1,9 +1,44 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import ArticlesListItem from '../ArticlesListItem'
+import './article-list.css'
 
 export default class ArticlesList extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      articlesList: []
+    }
+  }
+
+  componentDidMount () {
+    axios.get('http://liuziyang.top/api/article?page=1')
+      .then(res => {
+        if (res.data && res.data.code === 0) {
+          const data = res.data.data
+          this.setState({
+            articlesList: data
+          })
+        }
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
   render() {
+    const { articlesList } = this.state
     return (
-     <p>Articles List</p>
+     <div className="articles-list-container">
+      <ul className="articles-list">
+        {
+          articlesList && articlesList.map(item => (
+            <ArticlesListItem key={item['_id']} articlesItem={item} />
+          ))
+        }
+      </ul>
+     </div>
     );
   }
 }
