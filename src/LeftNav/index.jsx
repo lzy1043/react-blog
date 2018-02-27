a1eab1c9b00093fca03c5f49ac667ddc69cb56eb
liuziyang <798230885@qq.com>
2018-02-27 23:07:05 +0800

    add left nav component

---
+++ src/LeftNav/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './left-nav.css'
+
+export default class LeftNav extends Component {
+  render () {
+    return (
+      <div className="left-nav"></div>
+    )
+  }
+}
