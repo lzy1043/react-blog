bea46b7762a1ca84c4b64905dc15e4bf80fa4422
liuziyang <798230885@qq.com>
2018-03-01 23:41:14 +0800

    add article detail page

--- src/Articles/ArticleDetail/index.jsx
+++ src/Articles/ArticleDetail/index.jsx
@@ -1,9 +1,42 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import LeftNav from '../../LeftNav'
+import '../../assets/markdown.css'
+import '../../assets/github.css'
+import './article-detail.css'
 
 export default class ArticleDetail extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      articleDetail: ''
+    }
+  }
+
+  fetchArticleDetail (id) {
+    axios.get(`//liuziyang.top/api/article/${id}`)
+      .then(response => {
+        if (response.data && response.data.code === 0) {
+          this.setState({
+            articleDetail: response.data.data
+          })
+        }
+      })
+  }
+
+  componentDidMount () {
+    const articleId = this.props.match.params.id
+    this.fetchArticleDetail(articleId)
+  }
+
   render() {
+    const { articleDetail } = this.state
+    const html = {__html: articleDetail.htmlcont}
     return (
-     <p>Article Detail</p>
+     <div className="article-detail">
+      <div className="markdown-body" dangerouslySetInnerHTML={html}></div>
+      <LeftNav />
+     </div>
     );
   }
 }
