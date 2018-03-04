62ba287479686d2323503a3adcd3a0ab99ad089c
liuziyang <798230885@qq.com>
2018-03-04 19:26:48 +0800

    update home page

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,16 +1,44 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { API_BASE_URL } from '../config'
 import ArticleList from '../Articles/ArticlesList'
 import LeftNav from '../LeftNav'
 import Banner from '../Banner'
 import './home.css'
 
 export default class Home extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+      articlesList: []
+    }
+  }
+
+  componentDidMount () {
+    axios.get(`${API_BASE_URL}/article?page=1`)
+      .then(res => {
+        if (res.data && res.data.code === 0) {
+          const data = res.data.data
+          this.setState({
+            articlesList: data
+          })
+        }
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
   render () {
+    const { articlesList } = this.state
     return (
       <div className="home">
         <Banner />
-        <ArticleList />
-        <LeftNav />
+        <div className="home-wrapper">
+          <ArticleList articlesList={articlesList} />
+          <LeftNav />
+        </div>  
       </div>
     )
   }
