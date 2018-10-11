f07478795c0c06f1759df042cbb10f39c4d9aba5
liuziyang <ziyang.liu@tongfudun.com>
2018-10-11 15:08:42 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware  } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import reducers from './store/reducers'
 
 const store = createStore(reducers, applyMiddleware(thunkMiddleware))
-
-console.log(store.getState())
 
 ReactDOM.render(
   <Provider store={store}>
